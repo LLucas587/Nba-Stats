c71ece8007deba5a7ce833566ba986956539805e
LLucas587 <122763088+LLucas587@users.noreply.github.com>
2024-05-16 15:38:56 -0500

    added positional decision trees

--- combine_decision_tree.py
+++ combine_decision_tree.py
@@ -10,21 +10,38 @@ from sklearn import tree
 import seaborn as sns
 
 
-def import_data() -> pd.DataFrame:
-    """ Imports data from two datasets and adds a drafted column to the draft combine stats"""
+def import_data(position) -> pd.DataFrame:
+    """ Imports data from two datasets and adds a drafted column to the draft combine stats
+    
+    If position='ALL', includes all players.
+    Elseif position = 'C','PF','SF','SG' or 'PG', then only includes players from those positions
+    """
     df = pd.read_csv('DataSet/draft_combine_stats.csv') 
     draft_history = pd.read_csv('DataSet/draft_history.csv')
     draft_history = draft_history[draft_history['season']>=2001]
-    df = df[df['season']!=2023]
+    if position.upper() == 'ALL':
+         pass
+    elif position.upper() == 'C':
+         df = df[(df['position']=='C') | (df['position']=='PF-C') | (df['position']=='C-PF')]
+    elif position.upper() == 'PF':
+         df = df[(df['position']=='PF') | (df['position']=='PF-C') | (df['position']=='C-PF') | (df['position']=='PF-SF') | (df['position']=='SF-PF')]
+    elif position.upper() == 'SF':
+         df = df[(df['position']=='SF') | (df['position']=='SF-SG') | (df['position']=='SG-SF') | (df['position']=='PF-SF') | (df['position']=='SF-PF')]
+    elif position.upper() == 'SG':
+         df = df[(df['position']=='SG') | (df['position']=='SF-SG') | (df['position']=='SG-SF') | (df['position']=='PG-SG') | (df['position']=='SG-PG')]
+    elif position.upper() == 'PG':
+         df = df[(df['position']=='PG') | (df['position']=='PG-SG') | (df['position']=='SG-PG')]
+
     df['drafted'] = df['player_name'].isin(draft_history['player_name'])
     df = df[['season','player_name','position','height_wo_shoes','weight','wingspan','standing_reach','body_fat_pct','standing_vertical_leap','max_vertical_leap','bench_press','lane_agility_time','three_quarter_sprint','drafted']]
     return df
 
-def decision_tree():
+def decision_tree(position):
     """
-    Cleans data, splits data into train/test splits, and then makes decision trees using sklearn decision trees
+    position = 'ALL' or 'C','PF','PG','SG' or 'SF'
+    Cleans data, splits data into train/test splits, and then makes/plots decision trees and feature importance using sklearn decision trees
     """
-    df = import_data()
+    df = import_data(position.upper())
     x = df.iloc[:,3:13]
     y = df.iloc[:,13]
     x.dropna(inplace=True)#
@@ -54,12 +71,5 @@ def decision_tree():
     plt.bar(keys,values)
     plt.show()
 
-
-
-
-
-
-
-
-
-decision_tree()
+if __name__ == "__main__":
+     decision_tree('all')
